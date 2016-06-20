d57875caf309414113688acb080bd99ac7b1f5e6
Eliot BARDET <eliot.bardet@gmail.com>
2016-06-20 10:23:22 +0200

    customer resource added

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,31 @@
+class CustomersController < ApplicationController
+  before_action :set_customer, only: %i(show destroy)
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+
+    respond_to do |format|
+      if @customer.save
+        format.html { redirect_to '/' }
+        format.json { render :show, status: :created, location: @customer }
+      else
+        format.html { render :new }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:name, :email)
+  end
+end
